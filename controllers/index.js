8e2f55d09ef156025b8317be9cf780d635f004b4
kevingm007 <46567076+kevingm007@users.noreply.github.com>
2020-01-28 10:29:04 -0500

    first commit

---
+++ controllers/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'COMP2106 Global Food Market' });
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('about', {
+    title : 'About Us',
+    countries : [{
+      name: 'Canada'
+    }, {
+      name: 'India'
+    }, {
+      name: 'Italy'
+    }, {
+      name: 'Barbados'
+    }, {
+      name: 'Iran'
+    }, {
+      name: 'Taiwan'
+    }, {
+      name: 'Korea'
+    }, {
+      name: 'Vietnam'
+    }]
+
+  })
+})
+
+
+module.exports = router;
